0542e5e733c294ee3b3215055448fb58358d3183
jun.yin <jun.yin@caetar.com>
2022-02-17 15:48:25 +0800

    init commit

---
+++ acanTest/src/main/java/com/acantest/acanTest/AcanTestApplication.java
@@ -0,0 +1,13 @@
+package com.acantest.acanTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcanTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcanTestApplication.class, args);
+	}
+
+}
---
+++ acanTest/src/test/java/com/acantest/acanTest/AcanTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acantest.acanTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcanTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
